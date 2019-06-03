cd6404d6f4f46fd92489733b40960abf25f1b168
errikosmes <errikos.messara@gmail.com>
2019-06-03 16:53:36 +0200

    pop up reparé

--- fonctions.c
+++ fonctions.c
@@ -133,77 +133,29 @@ int rand_a_b(int a, int b){
 
 void pop_up(int** GRILLE){
   /*Fonction faisant apparaitre un 2 (7 chances sur 8) ou un 4 (1 chance sur 8) au hasard dans la GRILLE*/
-
-  int num;
-  int i=0;
-  int j=0;
-  int k=rand_a_b(0,20);
-
-  if(k<18) num=2;
-  else num=4;
-  while(k!=0){
-	if(GRILLE[i][j]==0){
-	  k--;
-	}
-	i++;
-	if(i==4){
-	  i=0;
-	  j++;  
-	}
-	if(j==4)
-	  j=0;
-   }
-   if(i!=0){
-     i--;
-   }
-   else {
-     j--;
-     i=3;
-   }
-
-   
-   GRILLE[i][j]=num;
-}
-
-   /*Fonction faisant apparaitre un 2 (7 chances sur 8) ou un 4 (1 chance sur 8) au hasard dans la GRILLE*/
-   /*
-  int num, i=0, j=0, indice_case=0;
+	int num, i=0, j=0, indice_case=0, nb_cases_vides=0, case_a_popup;
   int k=rand_a_b(0,19);
   
   if(k<18) num=2;
   else num=4;
-  
-  while(k!=0){
-	if(GRILLE[i][j]==0){
-	  k--;
-	}
-	
-	j++;
-	if(j==4){
-	  j=0;
-	  i++;  
-	}
-	if(i==4)
-	  i=0;
-  }
-  GRILLE[i][j]=num;
 
-	while(indice_case<k){
-		for(i=0; i<4; i++){
-			for(j=0; j<4; j++){
-				if (indice_case == k ){
-					if (GRILLE[i][j]!=0) {
-						GRILLE[i][j]=num;
-						return;
-					}
-					else k++;
-				}
-				indice_case++; 
-			}
+	for(i=0; i<4; i++){
+		for(j=0; j<4; j++){
+			if (GRILLE[i][j] == 0) nb_cases_vides++;
 		}
 	}
-}*/
 
+	case_a_popup = rand_a_b(0,nb_cases_vides);
+
+	for(i=0; i<4; i++){
+		for(j=0; j<4; j++){
+			if (case_a_popup == indice_case) {	
+				GRILLE[i][j]=num;
+			}
+			indice_case++;
+		}
+	}
+}
 
 
 int** init_grille(){
