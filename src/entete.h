03336c33407917a5bc18be0e627d2d43f3f906ab
errikosmes <errikos.messara@gmail.com>
2019-06-11 15:55:08 +0200

    fonctionnelle

---
+++ src/entete.h
@@ -0,0 +1,60 @@
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+gboolean on_key_press (GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+void on_btn_recommencer_clicked();
+void on_window_main_destroy();
+int** init_grille();
+void affichage_grille(int** GRILLE);
+int rand_a_b(int a, int b);
+void pop_up(int** GRILLE);
+void afficher_grille(int** GRILLE);
+int deplacement_possible_gauche(int** GRILLE, int** TEST);
+int deplacement_possible_droite(int** GRILLE, int** TEST);
+int deplacement_possible_haut(int** GRILLE, int** TEST);
+int deplacement_possible_bas(int** GRILLE, int** TEST);
+void fusion (int** GRILLE,int i1, int j1, int i2, int j2);
+void deplacement_case_bas(int** GRILLE, int i, int j, int** TEST);
+void deplacement_case_haut(int** GRILLE, int i, int j, int** TEST);
+void deplacement_case_gauche(int** GRILLE, int i, int j, int** TEST);
+void deplacement_case_droite(int** GRILLE, int i, int j, int** TEST);
+void deplacement_bas(int **GRILLE, int** TEST);
+void deplacement_haut(int **GRILLE, int** TEST);
+void deplacement_gauche(int **GRILLE, int** TEST);
+void deplacement_droite(int **GRILLE, int** TEST);
+int** cree_copie(int** GRILLE);
+int matrices_egales(int**COPIE,int** GRILLE);
+int etat_du_jeu(int** GRILLE, int** TEST);
+void tour_de_jeu(int** GRILLE, int** TEST);
+int** init_test();
+void free_test(int** TEST);
+void print_int_to_label(int valeur, GtkWidget* label);
+void print_int_to_grid_label(int valeur, GtkWidget* label);
+void fenetre_gagne();
+void fenetre_perdu();
+
+int **GRILLE;
+int **TEST;
+int SCORE;
+
+GtkWidget *g_lbl_score;
+GtkWidget *g_lbl_grid_0_0;
+GtkWidget *g_lbl_grid_0_1;
+GtkWidget *g_lbl_grid_0_2;
+GtkWidget *g_lbl_grid_0_3;
+GtkWidget *g_lbl_grid_1_0;
+GtkWidget *g_lbl_grid_1_1;
+GtkWidget *g_lbl_grid_1_2;
+GtkWidget *g_lbl_grid_1_3;
+GtkWidget *g_lbl_grid_2_0;
+GtkWidget *g_lbl_grid_2_1;
+GtkWidget *g_lbl_grid_2_2;
+GtkWidget *g_lbl_grid_2_3;
+GtkWidget *g_lbl_grid_3_0;
+GtkWidget *g_lbl_grid_3_1;
+GtkWidget *g_lbl_grid_3_2;
+GtkWidget *g_lbl_grid_3_3;
---
+++ src/main.c
@@ -0,0 +1,42 @@
+#include "entete.h"
+
+int main(int argc, char *argv[]){
+    GtkBuilder *builder;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    /*Initialisation des pointeurs des labels */
+    g_lbl_score = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_score"));
+    g_lbl_grid_0_0 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_0_0"));
+    g_lbl_grid_0_1 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_0_1"));
+    g_lbl_grid_0_2 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_0_2"));
+    g_lbl_grid_0_3 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_0_3"));
+	g_lbl_grid_1_0 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_1_0"));
+    g_lbl_grid_1_1 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_1_1"));
+    g_lbl_grid_1_2 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_1_2"));
+    g_lbl_grid_1_3 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_1_3"));
+	g_lbl_grid_2_0 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_2_0"));
+    g_lbl_grid_2_1 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_2_1"));
+    g_lbl_grid_2_2 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_2_2"));
+    g_lbl_grid_2_3 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_2_3"));
+	g_lbl_grid_3_0 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_3_0"));
+    g_lbl_grid_3_1 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_3_1"));
+    g_lbl_grid_3_2 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_3_2"));
+    g_lbl_grid_3_3 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_grid_3_3"));
+
+    GRILLE = init_grille();
+    affichage_grille(GRILLE);
+
+    g_object_unref(builder);
+    g_signal_connect (G_OBJECT (window), "key_press_event", G_CALLBACK (on_key_press), NULL);
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
